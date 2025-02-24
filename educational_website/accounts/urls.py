f6352eab65c793511296ee24906399180d12a118
AyushiAnand70047 <ayushianand088@gmail.com>
2025-02-24 12:44:31 +0530

    files updated

--- educational_website/accounts/urls.py
+++ educational_website/accounts/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('register/', views.register_page, name='register'),
     path('login/', views.login_user, name='login_user'),
-    # path('home/', views.home, name='home'),
 ]
