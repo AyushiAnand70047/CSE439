376c4a7981c849a46373847ae9292fedffbc4cac
AyushiAnand70047 <ayushianand088@gmail.com>
2025-04-19 12:47:11 +0530

    home page improved

--- educational_website/content/urls.py
+++ educational_website/content/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('home/',views.home,name='content_home'),
-    path('home/english/english.html',views.english),
-    path('home/hindi/hindi.html',views.hindi),
-    path('home/english/songs/',views.songs),
-    path('test/',views.test)
+    path('home/basics/alphabet/',views.alphabet),
+    path('home/basics/number/',views.number),
+    path('home/basics/table/',views.table),
+    # path('home/english/english.html',views.english),
+    # path('home/hindi/hindi.html',views.hindi),
+    # path('home/english/songs/',views.songs),
+    # path('test/',views.test)
 ]
--- educational_website/content/views.py
+++ educational_website/content/views.py
@@ -7,25 +7,34 @@ def test(request):
 def home(request):
     return render(request,'home.html')
 
-def english(request):
-    return render(request,'english/english.html')
+def alphabet(request):
+    return render(request,'basics/alphabet.html')
 
-def hindi(request):
-    return render(request,'hindi/hindi.html')
+def number(request):
+    return render(request,'basics/number.html')
 
-def songs(request):
-    songs = [
-        {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
-        {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
-        {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
-        {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
-        {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
-        {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
-        {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
-        {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
-        {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
-        {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
-        {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
-        {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
-    ]
-    return render(request,'english/songs.html',{'videos':songs})
+def table(request):
+    return render(request,'basics/table.html')
+
+# def english(request):
+#     return render(request,'english/english.html')
+
+# def hindi(request):
+#     return render(request,'hindi/hindi.html')
+
+# def songs(request):
+#     songs = [
+#         {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
+#         {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
+#         {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
+#         {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
+#         {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
+#         {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
+#         {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
+#         {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
+#         {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
+#         {'name': 'ABC Song', 'source': "https://www.youtube.com/embed/kpy6QEAuLJw?si=UhYZPNxlv5_yR74X"},
+#         {'name': 'Numbers Song', 'source': "https://www.youtube.com/embed/JT0MmZcJ2Vw?si=nC39IBQSLWUBGAIa"},
+#         {'name': 'Hello Song', 'source': "https://www.youtube.com/embed/gghDRJVxFxU?si=Vt39NETDuidhKM_m"},
+#     ]
+#     return render(request,'english/songs.html',{'videos':songs})
